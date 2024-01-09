061711e432adf6f7f2adf1d58bfca7dde4250e7e
Chandra Shekar <mchandrashekar842@gmail.com>
2024-01-09 20:16:04 +0530

    login page setup for taking input

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, TextField, Typography } from '@mui/material'
 
 const Auth = () => {
+
+  const [isSignedIn, setIsSignedIn] = useState(false)
+  const [inputs, setInputs] = useState({
+    name: "", email: "", password: ""
+  })
+
+  const handleChange = e => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputs)
+  }
+
   return (
     <div>
-      Autherntication Bro!!
+      <form action="#" onSubmit={handleSubmit}>
+        <Box 
+          display='flex' 
+          flexDirection={'column'} 
+          justifyContent={'center'} 
+          alignItems={'center'}
+          padding={3}
+          boxShadow='-10px 10px 15px #ccc'
+          margin='auto'
+          marginTop={5}
+          maxWidth={400}
+          borderRadius={5}
+        >
+          <Typography variant='h4' padding={3} textAlign={'center'}> { !isSignedIn ? 'Login' : 'SignUp'}</Typography>
+            { isSignedIn && <TextField 
+              placeholder='Name' 
+              value={inputs.name} 
+              margin='normal'
+              name='name'
+              onChange={handleChange}
+              />
+            }
+            <TextField 
+              type={'email'} 
+              placeholder='Email' 
+              value={inputs.email} 
+              margin='normal'
+              name='email'
+              onChange={handleChange}
+            />
+            <TextField 
+              type={'password'} 
+              placeholder='Password' 
+              value={inputs.password} 
+              margin='normal'
+              name='password'
+              onChange={handleChange}
+            />
+            <Button variant='contained' sx={{borderRadius: 3, marginTop: 2}} color='warning' type='submit'>Submit</Button>
+            <Button onClick={() => setIsSignedIn(!isSignedIn) } sx={{borderRadius: 3, marginTop: 3}}>Change to { isSignedIn ? 'Login' : 'SignUp'}</Button>
+        </Box>
+      </form>
     </div>
   )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import { AppBar, Box, Button, Tab, Toolbar, Typography, Tabs } from '@mui/material'
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+
+    const [value, setValue] = useState();
+    const isLoggedIn = useSelector(state => state.isLoggedIn)
+
   return (
     <AppBar position="sticky" sx={{background: "linear-gradient(90deg, rgba(226,217,36,1) 30%, rgba(94,138,48,1) 69%)"}}>
         <Toolbar>
             <Typography variant='h5'>Blog App</Typography>
-            <Box display='flex'>
-                <Tabs>
-                    <Tab label='Blogs' />
-                    <Tab label='My Blogs' />
-                    <Tab label='Add Blog' />
+            <Box display='flex' marginLeft='auto' marginRight={'auto'}>
+                <Tabs textColor='inherit' value={value} onChange={(e, val) => setValue(val)}>
+                    <Tab LinkComponent={Link} to='/blogs' label='Blogs' />
+                    <Tab LinkComponent={Link} to='/myblogs' label='My Blogs' />
+                    <Tab LinkComponent={Link} to='/blogs/add' label='Add Blog' />
                 </Tabs>
             </Box>
             <Box display='flex' marginLeft='auto' >
-                <Button variant='contained' color='info' sx={{margin: 1, borderRadius: 7}}>Login</Button>
-                <Button variant='contained' color='info' sx={{margin: 1, borderRadius: 7}}>SignUp</Button>
-                <Button variant='contained' color='info' sx={{margin: 1, borderRadius: 7}}>LogOut</Button>
+                { !isLoggedIn && <>
+                    <Button LinkComponent={Link} to='/auth' variant='contained' color='info' sx={{margin: 1, borderRadius: 7}}>Login</Button>
+                <Button LinkComponent={Link} to='/auth' variant='contained' color='info' sx={{margin: 1, borderRadius: 7}}>SignUp</Button>
+                </> }
+                {isLoggedIn && <Button LinkComponent={Link} to='/auth' variant='contained' color='info' sx={{margin: 1, borderRadius: 7}}>LogOut</Button>}
+                
             </Box>
         </Toolbar>
     </AppBar>
