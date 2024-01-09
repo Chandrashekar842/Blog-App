f9c5a3c95e09eee104f841dbe6c70d6363629d1c
Chandra Shekar <mchandrashekar842@gmail.com>
2024-01-09 09:45:39 +0530

    styles added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
+import Auth from './components/Auth'
+import Blogs from './components/Blogs'
+import Header from './components/Header'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import UserBlogs from './components/UserBlogs'
+import BlogDetails from './components/BlogDetails'
+import AddBlog from './components/AddBlog'
 
 function App() {
 
   return (
-    <div>
-      <h2>BLog App</h2>
-    </div>
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path='/auth' element={<Auth />}/>
+          <Route path='/blogs' element={<Blogs />}/>
+          <Route path='/myblogs' element={<UserBlogs/>}/>
+          <Route path='/myblogs/:id' element={<BlogDetails />}/>
+          <Route path='/blogs/add' element={<AddBlog />}/>
+        </Routes>
+      </main>
+    </React.Fragment>
   )
 }
 
