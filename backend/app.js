32d74b9906e507bbccc903c8d51f406f48fef0c5
Chandra Shekar <mchandrashekar842@gmail.com>
2024-01-10 16:44:53 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 import { userRouter } from './routes/user-routes.js'
 import { blogRouter } from './routes/blog-routes.js'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
